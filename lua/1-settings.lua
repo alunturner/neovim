79d2ab9a59d6e5fc867a0d0af1ed04a7782b120c
alunturner <alun.turner@googlemail.com>
2023-08-10 18:41:20 +0100

    neaten up comments in settings

--- lua/1-settings.lua
+++ lua/1-settings.lua
@@ -12,54 +12,47 @@ cmd[[filetype plugin on]]
 cmd[[autocmd FileType * setlocal formatoptions-=cro]]
 
 local options = {
-  -- Tabs {{{
+  -- Tabs
   expandtab = true,                 -- Use spaces by default
   shiftwidth = 4,                   -- Set amount of space characters, when we press "<" or ">"
   tabstop = 4,                      -- 1 tab equal 2 spaces
   smartindent = true,               -- Turn on smart indentation
-  -- }}}	
 
-  -- Clipboard {{{
+  -- Clipboard 
   clipboard = 'unnamedplus',        -- Use system clipboard
   fixeol = false,                   -- Turn off appending new line in the end of a file
-  -- }}}
 
-  -- Folding {{{
+  -- Folding
   foldmethod = 'syntax',
-  -- }}}
 
-  -- Search {{{
+  -- Search
   ignorecase = true,                -- Ignore case if all characters in lower case
   joinspaces = false,               -- Join multiple spaces in search
   smartcase = true,                 -- When there is a one capital letter search for exact match
   showmatch = true,                 -- Highlight search instances
-  -- }}}
 
-  -- Window {{{
+  -- Window
   splitbelow = true,                -- Put new windows below current
   splitright = true,                -- Put new vertical splits to right
-  -- }}}
 
-  -- Wild Menu {{{
+  -- Wild Menu 
   wildmenu = true,                  -- Tab autocomplete for commands in the bottom bar
   wildmode = "longest:full,full",
-  -- }}}
 
-  -- Appearance {{{
+  -- Appearance
   wrap = false,                     -- Turn word wrap off
   termguicolors = true,             -- Use plenty of colours
   number = true,                    -- Show line numbers
   relativenumber = true,            -- Show relative line numbers
   scrolloff = 8                     -- Scroll when cursor is within eight lines of top/bottom
-  --}}}
 }
 
 -- Set options from table
 for option, val in pairs(options) do
-	opt[option] = val
+  opt[option] = val
 end
 
--- Default Plugins {{{
+-- Default Plugins
 local disabled_built_ins = {
     "netrw",
     "netrwPlugin",
@@ -81,9 +74,7 @@ local disabled_built_ins = {
     "matchit"
 }
 
+-- Set disabled plugins from table
 for _, plugin in pairs(disabled_built_ins) do
     g["loaded_" .. plugin] = 1
 end
--- }}}
-
--- vim: tabstop=4 shiftwidth=4 expandtab syntax=lua foldmethod=marker foldlevelstart=1
