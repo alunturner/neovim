77c5237df04e1416953e7f6ad749c1b1a74d3e42
alunturner <alun.turner@googlemail.com>
2023-08-11 14:41:08 +0100

    slim down and sort out autocomplete mapping

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -4,7 +4,6 @@
 return {
     {
         'hrsh7th/nvim-cmp',
-        event = "InsertEnter",
         dependencies = {
             'L3MON4D3/LuaSnip',
             'saadparwaiz1/cmp_luasnip',
@@ -17,48 +16,34 @@ return {
         },
         config = function()
             local cmp = require('cmp')
+            local luasnip = require('luasnip')
 
-            cmp.setup{
+            require("luasnip/loaders/from_vscode").lazy_load()
+
+            cmp.setup({
                 snippet = {
                     expand = function(args)
-                        require'luasnip'.lsp_expand(args.body) -- Luasnip expand
+                        luasnip.lsp_expand(args.body)
                     end,
                 },
-
-                -- Mappings for cmp
-                mapping = {
-
-                    -- Autocompletion menu
-                    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i' }),
-                    ['<CR>'] = cmp.config.disable,                      -- Turn off autocomplete on <CR>
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Turn on autocomplete on <C-y>
-
-                    -- Use <C-e> to abort autocomplete
-                    ['<C-e>'] = cmp.mapping({
-                        i = cmp.mapping.abort(), -- Abort completion
-                        c = cmp.mapping.close(), -- Close completion window
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    ["<CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = false,
                     }),
-
-                    -- Use <C-p> and <C-n> to navigate through completion variants
-                    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-                    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-                },
-
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },                -- LSP
-                    { name = 'nvim_lsp_signature_help' }, -- LSP for parameters in functions
-                    { name = 'nvim_lua' },                -- Lua Neovim API
-                    { name = 'luasnip' },                 -- Luasnip
-                    { name = 'buffer' },                  -- Buffers
-                    { name = 'path' },                    -- Paths
-                    { name = "emoji" },                   -- Emoji
-                }, {
                 }),
-                formatting = {}
-            }
 
-            -- Add snippets from Friendly Snippets
-            require("luasnip/loaders/from_vscode").lazy_load()
-        end
-    },
-}
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'nvim_lsp_signature_help' },
+                    { name = 'nvim_lua' },
+                    { name = 'luasnip' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                    { name = "emoji" },},
+                })
+            end,
+        }
+    }
