936a9395d496df2d8e60232585e76d22ff76c0fe
alunturner <alun.turner@googlemail.com>
2023-08-11 11:31:05 +0100

    write theme config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,9 @@
-return {}
+return {
+    "tiagovla/tokyodark.nvim",
+    lazy = false,
+    config = function()
+        vim.g.tokyodark_enable_italic_comment = true
+        vim.g.tokyodark_enable_italic = true
+        vim.cmd("color tokyodark")
+    end
+}
