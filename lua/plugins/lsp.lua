44f9b6313966796c4dca5a74e349acc8d7b18cfe
alunturner <alun.turner@googlemail.com>
2023-08-11 12:40:01 +0100

    move extensions.treesitter to plugins.lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,4 +43,40 @@ return {
             }
         end
     },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            require('nvim-treesitter.configs').setup({
+
+                -- Needed parsers
+                ensure_installed = {
+                    "lua",
+                    "typescript",
+                    "javascript",
+                    "rust",
+                    "vim",
+                    "vimdoc"
+                },
+
+                -- Install all parsers synchronously
+                sync_install = false,
+
+                -- Подсветка
+                highlight = {
+                    -- Enabling highlight for all files
+                    enable = true,
+                    disable = {},
+                },
+
+                indent = {
+                    -- Disabling indentation for all files
+                    enable = false,
+                    disable = {},
+                }
+            })
+        end
+    },
 }
