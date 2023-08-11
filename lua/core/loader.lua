5f10822c9123125b6a600e5df0d5058765e99eaf
alunturner <alun.turner@googlemail.com>
2023-08-11 11:10:13 +0100

    reorder loader for flow

--- lua/core/loader.lua
+++ lua/core/loader.lua
@@ -17,15 +17,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- We have to set the leader key before loading plugins
+local keys = require("helpers.keys")
+keys.set_leader(" ")
+
 -- Use a protected call so we don't error out on first use
 local ok,lazy = pcall(require, "lazy")
 if not ok then
     return
 end
-
--- We have to set the leader key before loading plugins
-local keys = require("helpers.keys")
-keys.set_leader(" ")
 
 -- Load plugins from specifications
 lazy.setup("plugins")
