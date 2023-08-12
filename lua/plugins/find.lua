96d6d158e1eb560b80912afc22261c95aa495c12
alunturner <alun.turner@googlemail.com>
2023-08-12 08:22:09 +0100

    configure find plugin

--- lua/plugins/find.lua
+++ lua/plugins/find.lua
@@ -11,7 +11,26 @@ local Plugin = {
 }
 
 Plugin.config = function ()
-    require("telescope").setup()
+    require("telescope").setup({
+        defaults = {
+            mappings = {
+                i = {
+                    ["<C-j>"] = "move_selection_next",
+                    ["<C-k"] = "move_selection_previous",
+                    ["<Esc>"] = "close",
+                }
+            }
+        }
+    })
+
+    local map = require("utils.keys").map
+    local builtin = require('telescope.builtin')
+
+    map('n', '<leader>ff', builtin.find_files, "[f]ind [f]ile")
+    map('n', '<leader>fs', builtin.live_grep, "[f]ind [s]tring")
+    map("n", "<leader>fc", builtin.grep_string, "[f]ind at [c]ursor")
+    map("n", "<leader>fo", builtin.lsp_document_symbols, "[f]ind [o]bject")
+    map("n", "<leader>fh", builtin.help_tags, "[f]ind [h]elp")
 end
 
 return { Plugin }
