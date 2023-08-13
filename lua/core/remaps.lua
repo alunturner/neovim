af3b3f780105aa0cb243fb96b33685c3aec65792
alunturner <alun.turner@googlemail.com>
2023-08-13 15:11:57 +0100

    tidy up bindings

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -3,16 +3,14 @@
 local map = require("utils.keys").map
 
 -- Plugins
-map("n", "<leader>L", "<cmd>Lazy show<CR>", "Show the package manager")
+map("n", "<leader>L", "<cmd>Lazy show<CR>", "Show the [L]oader")
+
+-- Allow VSCode style shifting around of lines
+map("v", "J", ":m '>+1<CR>gv=gv", "Shift line(s) down")
+map("v", "K", ":m '<-2<CR>gv=gv", "Shift line(s) up")
 
 -- Quick way to remove highlighting
 map("n", "<leader>h", "<cmd>nohl<cr>", "Clear highlights")
-
--- Window navigation
-map("n", "<C-h>", "<C-w><C-h>", "Navigate windows to the left")
-map("n", "<C-j>", "<C-w><C-j>", "Navigate windows down")
-map("n", "<C-k>", "<C-w><C-k>", "Navigate windows up")
-map("n", "<C-l>", "<C-w><C-l>", "Navigate windows to the right")
 
 -- Buffer navigation
 map("n", "<leader>bp", "<cmd>:bprevious<cr>", "[b]uffer [p]revious")
@@ -26,6 +24,8 @@ map("n", "<leader>dn", vim.diagnostic.goto_next, "[d]iagnostic [n]ext")
 map("n", "<C-d>", "<C-d>zz", "Centre cursor after move")
 map("n", "<C-u>", "<C-u>zz", "Centre cursor after move")
 
--- Allow VSCode style shifting around of lines
-map("v", "J", ":m '>+1<CR>gv=gv", "Shift line(s) down")
-map("v", "K", ":m '<-2<CR>gv=gv", "Shift line(s) up")
+-- Window navigation
+map("n", "<C-h>", "<C-w><C-h>", "Move to window on left")
+map("n", "<C-j>", "<C-w><C-j>", "Move to window below")
+map("n", "<C-k>", "<C-w><C-k>", "Move to window above")
+map("n", "<C-l>", "<C-w><C-l>", "Move to window on right")
