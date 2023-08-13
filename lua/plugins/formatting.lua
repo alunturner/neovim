45f78f71d743c3d16360cf3eba1358169cea8e4a
alunturner <alun.turner@googlemail.com>
2023-08-13 11:53:33 +0100

    add all file types and parsers

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -9,18 +9,48 @@ local Plugin = {
 Plugin.config = function()
     require("formatter").setup({
         filetype = {
+            css = {
+                require("formatter.filetypes.javascript").prettierd,
+            },
+            html = {
+                require("formatter.filetypes.html").prettierd,
+            },
+            javascript = {
+                require("formatter.filetypes.javascript").prettierd,
+            },
+            javascriptreact = {
+                require("formatter.filetypes.javascriptreact").prettierd,
+            },
+            json = {
+                require("formatter.filetypes.json").prettierd,
+            },
             lua = {
                 require("formatter.filetypes.lua").stylua,
             },
+            markdown = {
+                require("formatter.filetypes.markdown").prettierd,
+            },
+            rust = {
+                require("formatter.filetypes.rust").rustfmt,
+            },
+            sh = {
+                require("formatter.filetypes.sh").shfmt,
+            },
+            typescript = {
+                require("formatter.filetypes.typescript").prettierd,
+            },
+            typescriptreact = {
+                require("formatter.filetypes.typescriptreact").prettierd,
+            },
             ["*"] = {
-                require("formatter.filetypes.any").remove_trailing_whitespace
-            }
-        }
+                require("formatter.filetypes.any").remove_trailing_whitespace,
+            },
+        },
     })
 
     local map = require("utils.keys").map
 
-    map('n', '<leader>s', '<cmd>Format<CR>', '[s]tyle my buffer')
+    map("n", "<leader>s", "<cmd>Format<CR>", "[s]tyle my buffer")
 end
 
 return { Plugin }
