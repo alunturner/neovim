a67e0fcfaa956c19262ce7e9ba136e8f7c90c023
alunturner <alun.turner@googlemail.com>
2023-08-13 12:44:40 +0100

    set bindings and sources for autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -2,66 +2,70 @@
 --!uses: nvim-cmp::hrsh7th/nvim-cmp
 
 local Plugin = {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     dependencies = {
         -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
 
-        -- Adds LSP completion capabilities
-        'hrsh7th/cmp-nvim-lsp',
+        -- Sources for the command line and search
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
 
-        -- Adds a number of user-friendly snippets
-        'rafamadriz/friendly-snippets',
+        -- Source for lsp output
+        "hrsh7th/cmp-nvim-lsp",
     },
 }
-Plugin.config = function()
-    local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    require('luasnip.loaders.from_vscode').lazy_load()
-    luasnip.config.setup {}
 
-    cmp.setup {
+Plugin.config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    -- configure autocomplete for lua, I think this is required for more than just lua
+    require("luasnip.loaders.from_vscode").lazy_load()
+    luasnip.config.setup({})
+
+    cmp.setup({
         snippet = {
             expand = function(args)
                 luasnip.lsp_expand(args.body)
             end,
         },
-        mapping = cmp.mapping.preset.insert {
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete {},
-            ['<CR>'] = cmp.mapping.confirm {
+        -- add basic mappings for position control and auto insertion of first item on <C-Space>
+        mapping = cmp.mapping.preset.insert({
+            ["<C-j>"] = cmp.mapping.select_next_item(),
+            ["<C-k>"] = cmp.mapping.select_prev_item(),
+            ["<Tab>"] = cmp.mapping.select_next_item(),
+            ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+            ["<C-Space>"] = cmp.mapping.confirm({
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
-            },
-            ['<Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                elseif luasnip.expand_or_locally_jumpable() then
-                    luasnip.expand_or_jump()
-                else
-                    fallback()
-                end
-            end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.locally_jumpable(-1) then
-                    luasnip.jump(-1)
-                else
-                    fallback()
-                end
-            end, { 'i', 's' }),
-        },
+            }),
+        }),
+        -- in a document, allow lsp and path candidates
         sources = {
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
+            { name = "nvim_lsp" },
         },
-    }
+    })
+
+    -- in the command line, allow path and command line candidates
+    cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+            { name = "path" },
+        }, {
+            { name = "cmdline" },
+        }),
+    })
+
+    -- in the search bar, allow buffer candidates
+    cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = "buffer" },
+        },
+    })
 end
 
 return { Plugin }
