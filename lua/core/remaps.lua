7599491bd368be9dc03c34e6744b3a9b783bb987
alunturner <alun.turner@googlemail.com>
2023-08-13 15:33:56 +0100

    improve remove highlighting shortcut

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -10,7 +10,7 @@ map("v", "J", ":m '>+1<CR>gv=gv", "Shift line(s) down")
 map("v", "K", ":m '<-2<CR>gv=gv", "Shift line(s) up")
 
 -- Quick way to remove highlighting
-map("n", "<leader>h", "<cmd>nohl<cr>", "Clear highlights")
+map("n", "<Esc>", "<cmd>nohl<cr>", "Clear highlights")
 
 -- Buffer navigation
 map("n", "<leader>bp", "<cmd>:bprevious<cr>", "[b]uffer [p]revious")
@@ -25,7 +25,7 @@ map("n", "<C-d>", "<C-d>zz", "Centre cursor after move")
 map("n", "<C-u>", "<C-u>zz", "Centre cursor after move")
 
 -- Window navigation
-map("n", "<C-h>", "<C-w><C-h>", "Move to window on left")
-map("n", "<C-j>", "<C-w><C-j>", "Move to window below")
-map("n", "<C-k>", "<C-w><C-k>", "Move to window above")
-map("n", "<C-l>", "<C-w><C-l>", "Move to window on right")
+map("n", "<C-h>", "<C-w>h", "Move to window on left")
+map("n", "<C-j>", "<C-w>j", "Move to window below")
+map("n", "<C-k>", "<C-w>k", "Move to window above")
+map("n", "<C-l>", "<C-w>l", "Move to window on right")
