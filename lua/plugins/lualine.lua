11510172b9e95596f9bc9d2f72f982185cdb7f96
alunturner <alun.turner@googlemail.com>
2023-08-17 07:15:09 +0100

    add personalised custom theme for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,48 @@
 --!structure::add status line
 
+-- try and smash in a local arctic theme
+local colors = {
+  pink = '#c586c0',
+  red = '#c72e0f',
+  orange = '#cc6633',
+  green = '#16825d',
+  blue = '#007acc',
+  violet = '#646695',
+  purple = '#68217a',
+  white = '#ffffff',
+  lightgray = '#454545',
+  gray = '#252526',
+}
+
+local custom_arctic = {
+  normal = {
+    a = { fg = colors.white, bg = colors.blue },
+    b = { fg = colors.white, bg = colors.lightgray },
+    c = { fg = colors.white, bg = colors.gray }
+  },
+  insert = {
+    a = { fg = colors.white, bg = colors.orange },
+  },
+  visual = {
+    a = { fg = colors.white, bg = colors.purple },
+  },
+  replace = {
+    a = { fg = colors.white, bg = colors.pink },
+  },
+  command = {
+    a = { fg = colors.white, bg = colors.green },
+  },
+  terminal = {
+    a = { fg = colors.white, bg = colors.violet },
+  },
+  pending = {
+    a = { fg = colors.white, bg = colors.red },
+  },
+  inactive = {
+    a = { fg = colors.white, bg = colors.darkgray },
+  }
+}
+
 local Plugin = {
     "nvim-lualine/lualine.nvim", --!uses::lualine
     requires = { "nvim-tree/nvim-web-devicons", opt = true },
@@ -8,6 +51,7 @@ local Plugin = {
 Plugin.config = function()
     require("lualine").setup({
         options = {
+            theme = custom_arctic,
             disabled_filetypes = {
                 statusline = {"no-neck-pain"}
             },
