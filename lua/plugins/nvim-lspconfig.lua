a6d913260061b6f9d7dfaf1c8e4143d45ad58dab
alunturner <alun.turner@googlemail.com>
2023-08-26 09:21:04 +0100

    add eslint server

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,12 @@ local Plugin = {
 
 Plugin.config = function()
     local lspconfig = require("lspconfig")
-    lspconfig.tsserver.setup({})
+
+    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
+    lspconfig.tsserver.setup({}) 
+
+    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#eslint
+    lspconfig.eslint.setup({}) 
 
     -- Global mappings.
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
