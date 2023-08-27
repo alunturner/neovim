571ede59ccf7eb2bf0ae50085152cbc1119ee586
alunturner <alun.turner@googlemail.com>
2023-08-27 11:33:25 +0100

    remove the logic around configuration

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -166,7 +166,8 @@ end
 local vscode = {}
 local config = {} -- nb this is required in the lualine theme file
 
-local defaults = {
+-- initialize the config to the defaults
+config.opts = {
     transparent = false,
     italic_comments = false,
     color_overrides = {},
@@ -174,28 +175,11 @@ local defaults = {
     disable_nvimtree_bg = true,
 }
 
-config.opts = {}
-
----@param user_opts? table
-config.setup = function(user_opts)
-    -- backwards compatibility: let users still set settings with global vars
-    local global_settings_opts = vim.tbl_extend("force", defaults, {
-        transparent = (vim.g.vscode_transparent == true or vim.g.vscode_transparent == 1),
-        italic_comments = (vim.g.vscode_italic_comment == true or vim.g.vscode_italic_comment == 1),
-        disable_nvimtree_bg = (vim.g.vscode_disable_nvim_tree_bg == true or vim.g.vscode_disable_nvim_tree_bg == 1),
-    })
-
-    -- but override global vars settings with setup() settings
-    config.opts = vim.tbl_extend("force", global_settings_opts, user_opts or {})
-
-    -- setting transparent to true removes the default background
-    if config.opts.transparent then
-        config.opts.color_overrides.vscBack = "NONE"
-    end
+-- setting transparent to true removes the default background
+if config.opts.transparent then
+    config.opts.color_overrides.vscBack = "NONE"
 end
 
--- initialize config
-config.setup()
 local hl = vim.api.nvim_set_hl
 local theme = {}
 
