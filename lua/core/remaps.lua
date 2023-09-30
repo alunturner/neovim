c6d966e29f0995814011cc9bfab72c886c0af534
alunturner <alun.turner@googlemail.com>
2023-09-30 12:18:35 +0100

    update remaps and lsp config

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -10,14 +10,6 @@ map("v", "K", ":m '<-2<CR>gv=gv", "Shift line(s) up")
 -- Quick way to remove highlighting
 map("n", "<Esc>", "<cmd>nohl<cr>", "Clear highlights")
 
--- Buffer navigation
-map("n", "<leader>bp", "<cmd>:bprevious<cr>", "[b]uffer [p]revious")
-map("n", "<leader>bn", "<cmd>:bnext<cr>", "[b]uffer [n]ext")
-
--- Diagnostic navigation
-map("n", "<leader>dp", vim.diagnostic.goto_prev, "[d]iagnostic [p]revious")
-map("n", "<leader>dn", vim.diagnostic.goto_next, "[d]iagnostic [n]ext")
-
 -- Keep cursor centred when moving the screen
 map("n", "<C-d>", "<C-d>zz", "Centre cursor after move")
 map("n", "<C-u>", "<C-u>zz", "Centre cursor after move")
